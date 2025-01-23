50e0edcc9619ea81fc2e72a986f91df593857fa8
wemeet-kay <kay@wemeetmobility.com>
2025-01-23 16:18:45 +0900

    2

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Static Export 활성화
-  trailingSlash: true, // GitHub Pages와 호환
+  trailingSlash: true, // GitHub Pages에서 404 방지
+  images: {
+    unoptimized: true, // GitHub Pages는 이미지 최적화를 지원하지 않으므로 설정 필요
+  },
 };
 
 export default nextConfig;
